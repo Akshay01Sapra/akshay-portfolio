d0b42af490f14b1cbaf8f3e0be020a9ed6ac448d
Akshay01Sapra <akshaysapra99@gmail.com>
2026-03-18 16:33:42 -0400

    design changes

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="row align-items-center">
           <div className="col-12 col-md-6">
                   <h3 className="md-text mb-3 body-heading">I'm a Web Designer.</h3>
-                  <p className="objective-text">I’m a <b> UI/UX Designer and Frontend Developer with 5+ years of experience creating scalable, user-centered digital products </b> across healthcare, e-commerce, and SaaS platforms. I specialize in transforming complex business requirements into intuitive interfaces that improve usability, accessibility, and product performance.</p>
+                  <p className="objective-text">I’m a <b> UI/UX Designer and Frontend Developer with 4+ years of experience creating scalable, user-centered digital products </b> across healthcare, e-commerce, and SaaS platforms. I specialize in transforming complex business requirements into intuitive interfaces that improve usability, accessibility, and product performance.</p>
                   <p className="objective-text">My work combines <b> design thinking with modern frontend engineering</b>, allowing me to move seamlessly from user research and information architecture to high-fidelity design and production-ready interfaces. I focus on building <b> responsive, high-performing web experiences </b> that balance visual clarity, usability, and technical efficiency.</p>
                   <p className="objective-text">I primarily work with <b> Figma, React.js, JavaScript, and modern CSS frameworks</b>, building reusable UI components and scalable design systems that help teams deliver consistent and efficient digital products.</p>
           </div>
